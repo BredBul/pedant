aeb11387b5d6d56ced162f07166ce7924f8ef849
BredBul <bulatzigangaraev@gmail.com>
2024-02-07 01:30:44 +0300

    formated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,22 @@
 module.exports = {
-    env: {
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        "plugin:vue/vue3-recommended",
-        "prettier"
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-    parser: "vue-eslint-parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        parser: "@typescript-eslint/parser",
-        sourceType: "module",
-    },
-    plugins: ["@typescript-eslint", "vue", "prettier"],
-    rules: {
-        "prettier/prettier": [
-            "error",
-            {
-                endOfLine: "auto",
-            },
-        ],
-        "vue/no-multiple-template-root": "off",
-    },
-}
+    "vue/no-multiple-template-root": "off",
+  },
+};
